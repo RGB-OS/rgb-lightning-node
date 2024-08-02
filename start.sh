a71dab4a9c8cd69ba1104020eeaf7511c69d81d2
Renat Gofman <renatgofman@gmail.com>
2024-08-02 11:46:56 +0300

    Fix S3FS mount

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Use the first argument specifically for `mount-s3` and then shift it out
-s3fs $1 /s3 -o iam_role=auto
+3fs $1 /s3 -o iam_role=auto -o ecs -o endpoint=us-east-2
 shift
 
 # Now, pass the remaining arguments to /usr/bin/rgb-lightning-node
