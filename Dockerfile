871120037f4d3fc55c9be6022c1786f46ab9493d
Renat Gofman <renatgofman@gmail.com>
2024-08-23 11:08:16 +0300

    fix mounting multiple ebs volumes

--- Dockerfile
+++ Dockerfile
@@ -9,8 +9,14 @@ FROM debian:bookworm-slim
 COPY --from=builder ./target/debug/rgb-lightning-node /usr/bin/rgb-lightning-node
 
 RUN apt-get update && apt install -y --no-install-recommends \
-    ca-certificates openssl wget s3fs bash \
+    ca-certificates openssl wget s3fs bash unzip curl \
     && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+# Install AWS CLI v2
+RUN wget "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -O "awscliv2.zip" \
+    && unzip awscliv2.zip \
+    && ./aws/install \
+    && rm -rf awscliv2.zip aws/
 
 COPY start.sh /
 
--- start.sh
+++ start.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
 # Use the first argument specifically for `mount-s3` and then shift it out
-3fs $1 /s3 -o iam_role=auto -o ecs -o endpoint=us-east-2
+s3fs $1 /s3 -o iam_role=auto -o ecs -o endpoint=us-east-2
 VOLUME_ID=$2
-
+USER_ID=$3
+NODE_ID=$4
 INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+
+# Find an available device name
+AVAILABLE_DEVICE=""
+for device in {f..z}; do
+    if ! lsblk | grep -q "/dev/xvd$device"; then
+        AVAILABLE_DEVICE="/dev/xvd$device"
+        break
+    fi
+done
+
+if [ -z "$AVAILABLE_DEVICE" ]; then
+    echo "Error: No available device name found."
+    exit 1
+fi
 
 # Detach the volume if it's already attached
 ATTACHMENT_STATE=$(aws ec2 describe-volumes --volume-ids $VOLUME_ID --query 'Volumes[0].Attachments[0].State' --output text)
@@ -20,19 +35,19 @@ if [ "$ATTACHMENT_STATE" == "attached" ]; then
 fi
 
 # Attach the volume to the current instance
-echo "Attaching volume $VOLUME_ID to instance $INSTANCE_ID"
-aws ec2 attach-volume --volume-id $VOLUME_ID --instance-id $INSTANCE_ID --device /dev/xvdh
+echo "Attaching volume $VOLUME_ID to instance $INSTANCE_ID as $AVAILABLE_DEVICE"
+aws ec2 attach-volume --volume-id $VOLUME_ID --instance-id $INSTANCE_ID --device $AVAILABLE_DEVICE
 while [ "$(aws ec2 describe-volumes --volume-ids $VOLUME_ID --query 'Volumes[0].Attachments[0].State' --output text)" != "attached" ]; do
     sleep 1
 done
 
 # Mount the volume
-if ! mount | grep /mnt/ebs; then
-    echo "Mounting volume $VOLUME_ID to /mnt/ebs-${var.user_id}-${each.key}"
-    mkdir -p /mnt/ebs-${var.user_id}-${each.key}
-    mount /dev/xvdh /mnt/ebs-${var.user_id}-${each.key}
+if ! mount | grep /mnt/ebs-${USER_ID}-${NODE_ID}; then
+    echo "Mounting volume $VOLUME_ID to /mnt/ebs-${USER_ID}-${NODE_ID}"
+    mkdir -p /mnt/ebs-${USER_ID}-${NODE_ID}
+    mount $AVAILABLE_DEVICE /mnt/ebs-${USER_ID}-${NODE_ID}
 fi
 
-shift 2
+shift 4
 # Now, pass the remaining arguments to /usr/bin/rgb-lightning-node
 /usr/bin/rgb-lightning-node $@
